dfdce350d5044f7c60ed5d6d77b2d51bfd0e827f
Mike Johnson <mike@failsafedesign.com>
2014-04-13 22:28:43 -0700

    add fade up images

--- app/assets/javascripts/projects.js
+++ app/assets/javascripts/projects.js
@@ -1,27 +1,23 @@
-var $grid = $('.filterable-grid');
-$grid.attr("style", "visibility:hidden");
+$(function(){ 
+  var $grid = $('.filterable-grid');
 
-function shuffle(filter){
-  $grid.shuffle({
-    itemSelector: '.project',
-    sizer: $grid.find('.shuffle__sizer'),
-    group: filter
+  $grid.imagesLoaded( function(){
+    $grid.shuffle({
+      itemSelector: '.project',
+      sizer: $grid.find('.shuffle__sizer'),
+      group: 'all'
+    });
+    $grid.attr("style", "opacity:1");
+    $('#loading').hide();
   });
-  $grid.attr("style", "visibility:visible");
-
-
-}
-
-$grid.imagesLoaded( function(){
-  shuffle('all');
-});
 
 
 
-var $filterItems = $('.filter .item'); 
-$filterItems.click( function(){
-  $filterItems.removeClass('active');
-  var $filter = $(this).attr('class').split(' ').last();
-  $(this).addClass('active');
-  $grid.shuffle('shuffle', $filter);
+  var $filterItems = $('.filter .item'); 
+  $filterItems.click( function(){
+    $filterItems.removeClass('active');
+    var $filter = $(this).attr('class').split(' ').last();
+    $(this).addClass('active');
+    $grid.shuffle('shuffle', $filter);
+  });
 });
