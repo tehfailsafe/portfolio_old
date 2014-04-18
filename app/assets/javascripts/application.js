924b46fa02497c60cb89425a0c3aabd86f9523aa
Mike Johnson <mike@failsafedesign.com>
2014-04-17 21:46:56 -0700

    update

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -1,10 +1,9 @@
-
 //= require_tree .
 
 function stickFooter(){
   var footer = $("#footer");
   var pos = footer.position();
-  var height = $(window).height();
+  var height = $('#container')[0].scrollHeight + 100;
   height = height - pos.top;
   height = height - footer.height();
   if (height > 0) {
@@ -13,25 +12,31 @@ function stickFooter(){
           'opacity': 1
       });
   }
-
 }
 
 
 
 $(function(){ 
   // $(document).foundation();
-
   var $grid = $('.filterable-grid'),
       $sizer = $grid.find('.shuffle__sizer');
+
+  $grid.on('done.shuffle layout.shuffle', function() {
+    stickFooter();
+  });
+
+  
   $grid.imagesLoaded( function(){
     $grid.shuffle({
       itemSelector: '.project',
       sizer: $sizer
     });
   });
+
+
   
 
-  setTimeout(stickFooter, 100);
+  // setTimeout(stickFooter, 100);
 
 
 });
--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -1,8 +1,10 @@
 class ProjectsController < ApplicationController
 
   def index
-    @tags = Tag.where('taggings_count > 0')
-    @projects = Project.order('sort ASC').all
+    # @tags = Tag.where('taggings_count > 0')
+    @tags = Tag.first
+    # @projects = Project.order('sort ASC').all
+    @projects = Project.first
   end
 
   def show
